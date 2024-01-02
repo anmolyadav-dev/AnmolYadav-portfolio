ea2bd9c37f9b65d66f154bbaf8c71f4084783b9e
Anmol Yadav <anmolyadav03unnao@gmail.com>
2024-01-03 02:13:26 +0530

    navbar added

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ window.addEventListener("load", () => {
   const loader = document.getElementById("loader");
   loader.classList.add("loader-hidden");
 });
+
+// NAvbar disapper
+const menuIcon = document.getElementById("menu-icon");
+var navbar = document.querySelector(".navbar");
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
+// remove navbar when clicked on any link
+
+var prevScrollPos = window.scrollY;
+
+function handleScroll() {
+  var currentScrollPos = window.scrollY;
+
+  if (prevScrollPos != currentScrollPos) {
+    navbar.classList.remove("active");
+  }
+}
+window.addEventListener("scroll", handleScroll);
